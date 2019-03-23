ed6e1e9a0e0e806ed1a14a44611f0932ebe686c3
Gunama Putra <gunama12@gmail.com>
2019-03-23 17:05:39 +0700

    Design topic/vote box And Handle onclick vote

--- src/App.js
+++ src/App.js
@@ -5,10 +5,31 @@ class Home extends Component {
 	constructor(props){
 		super(props);
 	}
+	handleClick(e, vote) {
+	    e.preventDefault();
+	    console.log(vote);
+	}
 	render(){
 		return (
-			<ul>
-			</ul>
+			<div className="App-home">
+				<h3>Topic List</h3>
+				<div className="App-topic">
+					<div className="App-vote-box">
+						<a href="#" onClick={(e) => this.handleClick(e, "up")}>▲</a>
+						<span>8</span>
+						<a href="#" onClick={(e) => this.handleClick(e, "down")}><span>▼</span></a>
+					</div>	
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor</p>
+				</div>
+				<div className="App-topic">
+					<div className="App-vote-box">
+						<a href="#" onClick={(e) => this.handleClick(e, "up")}>▲</a>
+						<span>8</span>
+						<a href="#" onClick={(e) => this.handleClick(e, "down")}><span>▼</span></a>
+					</div>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor</p>
+				</div>
+			</div>
 		);
 	}
 }
