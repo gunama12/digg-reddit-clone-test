a744c2a1e8ef1282b946aad2f004d71270b71619
Gunama Putra <gunama12@gmail.com>
2019-03-23 16:22:23 +0700

    Create Home Component And Create Data Structure use react state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+class Home extends Component {
+	constructor(props){
+		super(props);
+	}
+	render(){
+		return (
+			<ul>
+			</ul>
+		);
+	}
+}
+
 class App extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			topics: [
+				{
+					content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor",
+					vote: 0
+				}
+			]
+		}
+	}
   	render() {
 		return (
-	  		<div className="App">
+	  		<div>
+	  			<header className="App-header">
+	  				<h1>Digg/Reddit Clone</h1>
+	  				<nav></nav>
+	  			</header>
+	  			<Home/>
 		  	</div>
 		);
   	}
